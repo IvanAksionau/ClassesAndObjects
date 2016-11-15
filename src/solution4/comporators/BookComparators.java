e33d570692cf1ceb7f3c144f05eaee72d1f34487
IvanAksionau <Ivan_Aksionau@epam.com>
2016-11-15 18:04:02 +0300

    commit

--- src/solution4/comporators/BookComparators.java
+++ src/solution4/comporators/BookComparators.java
@@ -9,59 +9,59 @@ import java.util.Comparator;
  */
 public class BookComparators {
 
-   private static final Comparator<Book> bookTitleComparator = new Comparator<Book>() {
-      @Override
-      public int compare(Book o1, Book o2) {
-         return o1.getTitle().compareTo(o2.getTitle());
-      }
-   };
+    private static final Comparator<Book> bookTitleComparator = new Comparator<Book>() {
+        @Override
+        public int compare(Book o1, Book o2) {
+            return o1.getTitle().compareTo(o2.getTitle());
+        }
+    };
 
-   public static final Comparator<Book > bookTitleThenAuthorComparator = new Comparator<Book>() {
-      @Override
-      public int compare(Book o1, Book o2) {
-        int result =  o1.getTitle().compareTo(o2.getTitle());
-         if (result != 0) return result;
-         result = o1.getAuthor().compareTo(o2.getAuthor());
-         return result != 0 ? result : 0;
-      }
-   };
+    public static final Comparator<Book> bookTitleThenAuthorComparator = new Comparator<Book>() {
+        @Override
+        public int compare(Book o1, Book o2) {
+            int result = o1.getTitle().compareTo(o2.getTitle());
+            if (result != 0) return result;
+            result = o1.getAuthor().compareTo(o2.getAuthor());
+            return result != 0 ? result : 0;
+        }
+    };
 
-   private static final Comparator<Book> bookAuthorThenTitleComparator = new Comparator<Book>() {
-      @Override
-      public int compare(Book o1, Book o2) {
-         int result = o1.getAuthor().compareTo(o2.getAuthor());
-         if(result != 0) return result;
-         result = o1.getTitle().compareTo(o2.getTitle());
-         return result != 0 ? result : 0;
-      }
-   };
+    private static final Comparator<Book> bookAuthorThenTitleComparator = new Comparator<Book>() {
+        @Override
+        public int compare(Book o1, Book o2) {
+            int result = o1.getAuthor().compareTo(o2.getAuthor());
+            if (result != 0) return result;
+            result = o1.getTitle().compareTo(o2.getTitle());
+            return result != 0 ? result : 0;
+        }
+    };
 
-   private static final Comparator<Book> bookAuthorThenTitleThenPriceComparator = new Comparator<Book>() {
-      @Override
-      public int compare(Book o1, Book o2) {
-         int result = o1.getAuthor().compareTo(o2.getAuthor());
-         if(result != 0) return result;
-         result = o1.getTitle().compareTo(o2.getTitle());
-         if(result != 0) return result;
-         result = o1.getPrice() - o2.getPrice();
-         return result != 0 ? result : 0;
-      }
-   };
+    private static final Comparator<Book> bookAuthorThenTitleThenPriceComparator = new Comparator<Book>() {
+        @Override
+        public int compare(Book o1, Book o2) {
+            int result = o1.getAuthor().compareTo(o2.getAuthor());
+            if (result != 0) return result;
+            result = o1.getTitle().compareTo(o2.getTitle());
+            if (result != 0) return result;
+            result = o1.getPrice() - o2.getPrice();
+            return result != 0 ? result : 0;
+        }
+    };
 
-   public static Comparator<Book> getBookTitleComparator() {
-      return bookTitleComparator;
-   }
+    public static Comparator<Book> getBookTitleComparator() {
+        return bookTitleComparator;
+    }
 
-   public static Comparator<Book> getBookTitleThenAuthorComparator() {
-      return bookTitleThenAuthorComparator;
-   }
+    public static Comparator<Book> getBookTitleThenAuthorComparator() {
+        return bookTitleThenAuthorComparator;
+    }
 
-   public static Comparator<Book> getBookAuthorThenTitleComparator() {
-      return bookAuthorThenTitleComparator;
-   }
+    public static Comparator<Book> getBookAuthorThenTitleComparator() {
+        return bookAuthorThenTitleComparator;
+    }
 
-   public static Comparator<Book> getBookAuthorThenTitleThenPriceComparator() {
-      return bookAuthorThenTitleThenPriceComparator;
-   }
+    public static Comparator<Book> getBookAuthorThenTitleThenPriceComparator() {
+        return bookAuthorThenTitleThenPriceComparator;
+    }
 
 }
